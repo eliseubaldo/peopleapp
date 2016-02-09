047addfcb5e80a5842fbe55bc28438593f84ba84
eliseubaldo <eliseu_baldo@hotmail.com>
2016-02-09 11:12:05 -0500

    adjusted DI

--- app/controllers/peopleController.js
+++ app/controllers/peopleController.js
@@ -1,8 +1,8 @@
-peopleApp.controller('peopleController', function($scope, getPeopleList){
+peopleApp.controller('peopleController', ['$scope', 'getPeopleList', function($scope, getPeopleList){
 
 	getPeopleList.getList().success(function(data){
 			$scope.peopleList = data;
 		});
 	
 
-});
+}]);
