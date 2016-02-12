e816edf3590d8c9f8f617c3e7598938004e71714
eliseubaldo <eliseu_baldo@hotmail.com>
2016-02-12 15:57:42 -0500

    todo list

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -1,8 +1,25 @@
 peopleApp.controller('todoController',['$scope', function($scope){
 
-	$scope.saveItem = function(){
-		localStorage.setItem("todo #", "Smith");
+	$scope.todoList = [];
 
+	for (var i = 1; i < Infinity; i++) {
+		 if(localStorage.getItem("todo"+i) == null){
+		 	$scope.n=i-1;
+		 	break;
+		 }else {
+		$scope.todoList.push({"todo": localStorage.getItem("todo"+i)});
+		$scope.n=i;
+		}
+	};
+
+	$scope.saveItem = function(todoitem){
+		$scope.n++;
+		localStorage.setItem("todo"+$scope.n, todoitem);
+		var objToAdd = {"todo" : todoitem};
+		$scope.todoList.push(objToAdd);
+		//todoList["todo"+n] = todoitem;
+		//todoList.push({"todo": todoitem});
+		//console.log($scope.todoList);
 	}
 
 }]);
